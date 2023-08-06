99fdc96ab2efa37ce5572cfaaaf9f278ab84f76e
Gazerb <>
2023-08-06 13:11:38 +0000

    models added for three tables, one for students, one for Grades and one for attendance. Views for students page have also been created

--- students/models.py
+++ students/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Students'
+
+
+    name = models.CharField(max_length=254)
+    university = models.CharField(max_length=254)
+    major = models.CharField(max_length=254)
+    student_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Grades(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Student Grades'
+
+    name = models.ForeignKey('students', null=True, blank=True, on_delete=models.SET_NULL)
+    subject = models.CharField(max_length=254)
+    grade_score = models.IntegerField()
+
+    def __str__(self):
+        return self.subject
+
+
+class Attendance(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Attendance Scores'
+
+
+    name = models.ForeignKey('students', null=True, blank=True, on_delete=models.SET_NULL)
+    subject = models.ForeignKey('grades', null=True, blank=True, on_delete=models.SET_NULL)
+    attendance_score = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.attendance_score
--- students/views.py
+++ students/views.py
@@ -1,3 +1,22 @@
+# External Imports
 from django.shortcuts import render
+from .models import Students, Grades, Attendance 
 
-# Create your views here.
+# Internal Imports
+from . import views
+
+
+def all_students(request):
+    """A view to load student page"""
+
+    students = Students.objects.all()
+    grades = Grades.objects.all()
+    attendance = Attendance.objects.all()
+
+    context = {
+        'students' : students,
+        'grades' : grades,
+        'attendance' : attendance,
+    }
+
+    return render(request, "students/students.html", context)
