ee314b79c35a5848825480b0bc65285501211e06
Gazerb <>
2023-08-06 13:17:31 +0000

    changed admin.py in students app to show more details once in the admin section of the product

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from .models import Students, Grades, Attendance
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        'student_id',
+        'name',
+        'university',
+        'major',
+    )
+
+    ordering = ('student_id',)
+
+class GradesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'subject',
+        'grade_score',
+    )
+
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'subject',
+        'attendance_score',
+    )
+
 # Register your models here.
+admin.site.register(Students, StudentAdmin)
+admin.site.register(Grades, GradesAdmin)
+admin.site.register(Attendance, AttendanceAdmin)
